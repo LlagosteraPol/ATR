dcf70f9f4b0e483514da4660c5d049d6ed7a53c2
Pol Llagostera Blasco <plb92@hotmail.com>
2022-09-19 18:29:29 +0200

    Updated setup and __init__ files

--- atr/__init__.py
+++ atr/__init__.py
@@ -1,1 +1,5 @@
 __author__ = 'Pol Llagostera Blasco'
+from atr import atr
+from atr import graphtools
+from atr import selector
+
--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ from setuptools import setup
 setup(
     name='atr',
     version='1.0.0',
-    packages=['atr', 'atr.relmodules'],
-    url='https://github.com/LlagosteraPol/ATR',
-    license='MIT License',
     author='Pol Llagostera Blasco',
     author_email='llagosterapol.work@gmail.com',
-    description='This library provides a tool to calculate the all-terminal reliability polynomial of any undirected graph.'
+    packages=['atr', 'atr.relmodules'],
+    url='https://github.com/LlagosteraPol/ATR',
+    license='LICENSE.txt',
+    description='This library provides a tool to calculate the all-terminal reliability polynomial of any undirected graph.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+           "networkx >= 2.5.1",
+           "sympy >= 1.4.0",
+           "numpy >= 1.21.6"
+       ],
 )
