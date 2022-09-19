56192fc7a1c38a3a2a3ea2730204f9cf1e6a924c
Pol Llagostera Blasco <plb92@hotmail.com>
2022-09-19 20:10:04 +0200

    - Fixed imports in base init file

--- atr/__init__.py
+++ atr/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Pol Llagostera Blasco'
-from main import calculate_reliability
+from atr.main import calculate_reliability
 from atr import graphtools
 from atr import selector
 from atr import relmodules
